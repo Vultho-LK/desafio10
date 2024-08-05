0e317c8188fe7ee1fa37ee12c795b78ad75f013a
Hemerson Luan <140278282+Vultho-LK@users.noreply.github.com>
2024-08-04 21:38:23 -0300

    Add files via upload

---
+++ Desafio10.py
@@ -0,0 +1,3 @@
+# converter real em dolar 
+n1 = float(input('quanto de dinheiro você tem? em R$ '))
+print('R${:.2f} seria ${:.2f} em dolar \nem 04/08/2024'.format(n1, n1 / 5.73))
---
+++ README.md
@@ -0,0 +1,3 @@
+Converte real em dolar
+
+<img src=https://github.com/user-attachments/assets/1c54f799-6bd2-4942-9cd9-f47da9301488 width=100>
